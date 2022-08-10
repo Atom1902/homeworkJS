e5a5bfc35b4d76e0f159594832726f1ac48ec88f
Atom1902 <atom1902@gmail.com>
2022-08-10 12:18:02 +0300

    homeworkJS6

--- homeworkJS6/js/main.js
+++ homeworkJS6/js/main.js
@@ -1,158 +1,243 @@
-// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
 
-// function areaRectangle(a, b) {
-//         let res = (a + b)*2;
-//         return res;
+// let str = 'hello world';
+// console.log(str.length);
+
+// let str = 'lorem ipsum';
+// console.log(str.length);
+
+// let str = 'javascript is cool';
+// console.log(str.length);
+
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+// let str = 'hello world';
+// console.log(str.toUpperCase());
+
+// let str = 'lorem ipsum';
+// console.log(str.toUpperCase());
+
+// let str = 'javascript is cool';
+// console.log(str.toUpperCase());
+
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+// let str = 'HELLO WORLD';
+// console.log(str.toLowerCase());
+
+// let str = 'LOREM IPSUM';
+// console.log(str.toLowerCase());
+
+// let str = 'JAVASCRIPT IS COOL';
+// console.log(str.toLowerCase());
+
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+// let str = ' dirty string   ';
+// console.log(str.slice(1, 13));
+
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+
+// let str = 'Ревуть воли як ясла повні';
+// let stringToarray = str.split(' ')
+// console.log(stringToarray);
+
+
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+
+// const array1 = [10,8,-7,55,987,-1011,0,1050,0];
+// const map1 = array1.map(value => value);
+// console.log(map1);
+
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+
+// const sortNums = [11,21,3];
+// sortNums.sort(function (a, b) {
+//     return a - b;
+// });
+// console.log(numbers);
+
+
+
+
+// let sortNums = [11,21,3];
+// sortNums.sort(function (a, b) {
+//     return b - a;
+// });
+// console.log(sortNums);
+
+
+
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// -- відсортувати його за спаданням за monthDuration
+
+
+// coursesAndDurationArray.sort((a,b)=>b.monthDuration - a.monthDuration);
+// console.log(coursesAndDurationArray);
+
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+
+// function filterByMonthDuration(item) {
+//     if (item.monthDuration > 5) {
+//         return true
 //     }
-//     let x = areaRectangle(2, 3);
-//     console.log (x);
-
-    
-// - створити функцію яка обчислює та повертає площу кола з радіусом r
-
-// function areaCircle(p) {
-//        let res = p + 3.14**2;
-//     return res;
 // }
-// let q = areaCircle(2);
-// console.log (q);
-
-
-// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-
-// function areaCylinder(R, H) {
-//     let res =  2*3.14*(R**2)+ 2*3.14*R*H;
-//  return res;
-// }
-// let a = areaCylinder(1, 1);
-// console.log (a);
+// let arrByID = coursesAndDurationArray.filter(filterByMonthDuration)
+// console.log(arrByID)
 
 
 
-// - створити функцію яка приймає масив та виводить кожен його елемент
 
-    
-//     function  elementOfArray(arr)
-//     {
-//      for(i = 0; i < arr.length; i++)
-//             element = arr[i];
-//         return element;
+// описати колоду карт
+// let cards = [
+//     {value: 'joker'},
+//     {value: 'joker'},
+//     {cardSuit: 'heart',value: '6', color: 'red'},
+//     {cardSuit: 'heart',value: '7', color: 'red'},
+//     {cardSuit: 'heart',value: '8', color: 'red'},
+//     {cardSuit: 'heart',value: '9', color: 'red'},
+//     {cardSuit: 'heart',value: '10', color: 'red'},
+//     {cardSuit: 'heart',value: 'ace', color: 'red'},
+//     {cardSuit: 'heart',value: 'jack', color: 'red'},
+//     {cardSuit: 'heart',value: 'queen', color: 'red'},
+//     {cardSuit: 'heart',value: 'king', color: 'red'},
+//
+//     {cardSuit: 'diamond',value: '6', color: 'red'},
+//     {cardSuit: 'diamond',value: '7', color: 'red'},
+//     {cardSuit: 'diamond',value: '8', color: 'red'},
+//     {cardSuit: 'diamond',value: '9', color: 'red'},
+//     {cardSuit: 'diamond',value: '10', color: 'red'},
+//     {cardSuit: 'diamond',value: 'ace', color: 'red'},
+//     {cardSuit: 'diamond',value: 'jack', color: 'red'},
+//     {cardSuit: 'diamond',value: 'queen', color: 'red'},
+//     {cardSuit: 'diamond',value: 'king', color: 'red'},
+//
+//     {cardSuit: 'spade',value: '6', color: 'black'},
+//     {cardSuit: 'spade',value: '7', color: 'black'},
+//     {cardSuit: 'spade',value: '8', color: 'black'},
+//     {cardSuit: 'spade',value: '9', color: 'black'},
+//     {cardSuit: 'spade',value: '10', color: 'black'},
+//     {cardSuit: 'spade',value: 'ace', color: 'black'},
+//     {cardSuit: 'spade',value: 'jack', color: 'black'},
+//     {cardSuit: 'spade',value: 'queen', color: 'black'},
+//     {cardSuit: 'spade',value: 'king', color: 'black'},
+//
+//     {cardSuit: 'clubs',value: '6', color: 'black'},
+//     {cardSuit: 'clubs',value: '7', color: 'black'},
+//     {cardSuit: 'clubs',value: '8', color: 'black'},
+//     {cardSuit: 'clubs',value: '9', color: 'black'},
+//     {cardSuit: 'clubs',value: '10', color: 'black'},
+//     {cardSuit: 'clubs',value: 'ace', color: 'black'},
+//     {cardSuit: 'clubs',value: 'jack', color: 'black'},
+//     {cardSuit: 'clubs',value: 'queen', color: 'black'},
+//     {cardSuit: 'clubs',value: 'king', color: 'black'}
+//
+// ]
+
+
+// - знайти піковий туз
+
+// function filterByTuz(item) {
+//     if (item.cardSuit == 'spade' && (item.value == 'ace')) {
+//         return true
 //     }
-     
-//    let k = [1, 2, 3, 4, 5, 6, 7];
-//     console.log(k);
-    
+// }
+// let arrByID = cards.filter(filterByTuz)
+// console.log(arrByID)
 
-// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+// - всі шістки
 
-
-// function pWithText(Text) {
-//         document.write(`<p>${Text}</p>`);
+// function filterByNumber(item) {
+//     if (item.value == 6) {
+//         return true
 //     }
-//     pText('Lorem ipsum, dolor sit amet consectetur adipisicing elit.)
-
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-
-// function lishka(Text) {
-//     document.write(`
-//     <ul>
-//     <li>${Text}</li>
-//     <li>${Text}</li>
-//     <li>${Text}</li>
-//     </ul>`);
 // }
-// lishka('Lorem ipsum, dolor sit amet consectetur adipisicing elit.')
+// let arrByID = cards.filter(filterByNumber)
+// console.log(arrByID)
 
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-// function lishka(Text, b) {
-
-//   for(i = 0; i < arr.length; i++)
-
-//     document.write(`
-//     <ul>
-//     <li>${Text}</li>
-//     <li>${Text}</li>
-//     <li>${Text}</li>
-//     </ul>`);
-
+// - всі червоні карти
+// function filterByRed(item) {
+//     if (item.color == 'red') {
+//         return true
+//     }
 // }
-// lishka('Lorem ipsum, dolor sit amet consectetur adipisicing elit.')
+// let arrByID = cards.filter(filterByRed)
+// console.log(arrByID)
 
 
-// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-// let k = [1, 'text', false];
-//     function  elementOfArray(arr)
-//     {
-//              for(i = 0; i < arr.length; i++)
-//                     element = arr[i];
-//                 return element;
-//             }
-    
-//     document.write(`
-//     <ul>
-//     <li>${k[0]}</li>
-//     <li>${k[1]}</li>
-//     <li>${k[2]}</li>
-//     </ul>`);
+// - всі буби
+
+// function filterByDiamond(item) {
+//     if (item.cardSuit == 'diamond') {
+//         return true
+//     }
+// }
+// let arrByID = cards.filter(filterByDiamond)
+// console.log(arrByID)
+
+// - всі трефи від 9 та більше
+
+// function filterByNumber(item) {
+//     if (item.cardSuit == 'clubs' && (item.value > 9)) {
+//         return true
+//     }
+// }
+// let arrByID = cards.filter(filterByNumber)
+// console.log(arrByID)
+
+// А ЯК ТУТУ ДАТИ ЗРОЗУМІТИ ПРОГРАМІ ЩО ДАМА ТУЗ І ТД ВИЩІ ЗА 9??
+
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+//     color:'', // 'red','black'
+// }
+//
+//
+// Додатково по reduce
+// Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
+// {
+//     spades:[],
+//         diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// }
 
 
-  
-   
-// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-  // let users = [
-  //       {id:10, name: 'vasya', age: 31},
-  //       {id:15, name: 'petya', age: 30},
-  //       {id:22, name: 'kolya', age: 29},
-        
-  //   ];
-  //   function  objectOfArray(arr)
-  //   {
-  //   for(let i = 0; i < arr.length; i++)
-                           
-  //   document.write(`<div>id:${users[i].id},name:${users[i].name}, age:${users[i].age}</div>`);
-   
-  // }
-  // objectOfArray(users)
 
-// - створити функцію яка повертає найменьше число з масиву
-// перебиранням то канєшна капець..
-
-
-// const numbers = [3, 4, 9, 3, 5, 1, 7]; 
-// function arrayMin(array){
-
-// let minimum = numbers[0];
-// for (const item of numbers) {
-//   if(item < minimum) {
-//     minimum = item; 
+// let reduce = cards.reduce(function (accu,card ) {
+//   if (card.cardSuit== 'spade') {
+//       accu.Spade.push(card)
 //   }
-// }
-// console.log(minimum); 
-// }
-// arrayMin(numbers);
-
-// Простіше методом..
-
-
-
-// let arr = [3,2,5,6];
-// function arrayMin(array){
-// let min = 0;
-// for(var i = 0; i < array.length; i++){
-//     min = Math.min (...arr);
+//     if (card.cardSuit== 'heart') {
+//         accu.Heart.push(card)
 //     }
-// console.log(min);
-// }
-// arrayMin(arr);
-
-
-// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
-// let arr = [3,2,5,6];
-// function arraySum(array){
-// let sum = 0;
-// for(var i = 0; i < array.length; i++){
-//     sum += array[i];
+//     if (card.cardSuit== 'clubs') {
+//         accu.Clubs.push(card)
 //     }
-// console.log(sum);
-// }
-// arraySum(arr);
+//     if
+//       (card.cardSuit== 'diamond')
+//           accu.Diamonds.push(card)
+//
+//
+//   return accu;
+//
+// },{Spade:[], Heart:[], Clubs:[], Diamonds :[],});
+// console.log(reduce)
